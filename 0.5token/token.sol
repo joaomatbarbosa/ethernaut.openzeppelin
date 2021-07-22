c13368812ba9b4e2fcca2ba3d2d51d0a21e2b5f8
João Mateus <82168292+joaomatbarbosa@users.noreply.github.com>
2021-07-22 19:10:25 +0100

    Create token.sol

---
+++ 0.5token/token.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+// In this level, I had to perform an underflow calling the transfer function and using the value 20 + 1 in order to trigger it.
+contract Token {
+
+  mapping(address => uint) balances;
+  uint public totalSupply;
+
+  constructor(uint _initialSupply) public {
+    balances[msg.sender] = totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint _value) public returns (bool) {
+    require(balances[msg.sender] - _value >= 0);
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    return true;
+  }
+
+  function balanceOf(address _owner) public view returns (uint balance) {
+    return balances[_owner];
+  }
+}
